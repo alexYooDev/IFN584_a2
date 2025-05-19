0c6192e29b78f1bd55f18763f1e25d9f5f6acb9b
d_flex <hiyoo95@gmail.com>
2025-05-19 22:31:52 +1000

    comments edited

--- GameFrameWork/AbstractBoard.cs
+++ GameFrameWork/AbstractBoard.cs
@@ -3,8 +3,10 @@ namespace GameFrameWork
     public abstract class AbstractBoard
     {
         protected int Size { get; set; }
+
+        // Default number of board = 1, NoTakTo uses 3
         protected int BoardCount { get; set; } = 1;
-        protected List<object> BoardsState { get; set; }// Default number of board = 1, NoTakTo uses 3
+        protected List<object> BoardsState { get; set; }
 
         /* Constructor */
         public AbstractBoard(int size, int boardCount = 1)
@@ -19,24 +21,32 @@ namespace GameFrameWork
 
         public abstract void DisplayBoard();
         public abstract bool IsValidMove(int boardIndex, int row, int col, object moveData);
+
         /* 
-            boardIndex: the number of board, added in Notakto for board navigation
+            boardIndex: the number of board, added in Notakto for board navigation, set as 0 by default
             row : row number
             col : column number
             moveDate : to store / save information of the board every time player makes move
          */
-        public abstract void MakeMove(int boardIndex, int row, int col, object moveData = null);
+        public abstract void MakeMove(int boardIndex = 0, int row, int col, object moveData = null);
+
+        /* boardIndex: the number of board, added in Notakto for board navigation, set as 0 by default */
         public abstract bool IsBoardFull(int boardIndex = 0);
+
+        /* For Notakto to determine if all 3 boards are full */
         public abstract bool AreAllBoardsFull();
         public abstract object GetBoardState(); // Needed for saving/loading games
         public abstract void SetBoardState(object state); // Needed for restoring games
 
+        /* Getter for board size */
         public int GetSize()
         {
             return Size;
         }
+        
+        /* Getter for the number of board */
         public int GetBoardCount()
-        { 
+        {
             return BoardCount;
         }
     }
