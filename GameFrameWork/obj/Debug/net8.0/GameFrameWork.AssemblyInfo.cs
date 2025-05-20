b9b215f8a500d7e8ce41d52dadd3a78270b01977
d_flex <hiyoo95@gmail.com>
2025-05-21 00:31:11 +1000

    numerical tictactoe migrated to new framework

--- GameFrameWork/obj/Debug/net8.0/GameFrameWork.AssemblyInfo.cs
+++ GameFrameWork/obj/Debug/net8.0/GameFrameWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameFrameWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d7a0c3ed62f51c80ee62e900cbda940f0520eea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893f2e353fc760ad3c54b8d9897250bf126b0fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameFrameWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameFrameWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
